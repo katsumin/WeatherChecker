9be7b02ca5e8d2d37e85251a0d258ae33de85e01
katsumin <katsumi.noguchi@gmail.com>
2022-05-22 16:50:46 +0900

    refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -119,8 +119,9 @@ void setup()
   display.setTextDatum(textdatum_t::top_left);
   display.drawString(buf, 0, 0);
   display.drawRoundRect(0, 20, LCD_WIDTH, LCD_HEIGHT - 20 * 2, 5);
-  display.drawFastVLine(LCD_WIDTH / 2, 20, LCD_HEIGHT - 20 * 2, 0xD600);
-  display.drawFastHLine(0, 40, LCD_WIDTH, 0xD600);
+  display.drawFastVLine(LCD_WIDTH / 2, 20, LCD_HEIGHT - 20 * 2, TFT_YELLOW);
+  display.drawFastHLine(0, 40, LCD_WIDTH, TFT_LIGHTGRAY);
+  display.drawFastHLine(0, 60, LCD_WIDTH, TFT_LIGHTGRAY);
   // display.drawFastHLine(0, (LCD_HEIGHT - 20) - 80, LCD_WIDTH, 0xD600);
 
   if ((Ethernet.getChip() == w5500) || (Ethernet.getAltChip() == w5100s))
@@ -153,13 +154,123 @@ void setup()
 const char host[] = "www.jma.go.jp";
 SSLClient cl(ecl, TAs, (size_t)TAs_NUM, 0);
 StaticJsonDocument<8192> doc;
-char bufTime[32];
 char weather_json[64];
 
 const pin_size_t button_pins[] PROGMEM = {BTN_A, BTN_B, BTN_X, BTN_Y, JOY_UP, JOY_DOWN, JOY_LEFT, JOY_RIGHT, JOY_CTRL};
 const char *button_names[] PROGMEM = {"A", "B", "X", "Y", "UP", "DOWN", "LEFT", "RIGHT", "CTRL"};
 PinStatus button_state[sizeof(button_pins)][3];
 uint16_t timer_bklt = 60 * 3;
+
+#define POS_Y_TIME (20)
+#define POS_Y_CODE (40)
+#define POS_Y_MSG (60)
+#define HEIGHT_TIME (20)
+#define HEIGHT_CODE (20)
+#define WIDTH_TIME (LCD_WIDTH / 2)
+#define WIDTH_MSG (LCD_WIDTH / 2)
+#define HEIGHT_IMAGE (80)
+#define MAXLEN_MSG (3 * 7)
+#define TEXT_MARGIN (2)
+
+/**
+ * @brief 日付表示
+ *
+ * @param index 天気情報インデックス
+ * @param time 日付文字列
+ */
+void outputTime(int index, String time)
+{
+  int32_t img_x = (LCD_WIDTH / 4) + WIDTH_TIME * index;
+  // int32_t tw = display.textWidth(time);
+  display.fillRect(WIDTH_TIME * index + TEXT_MARGIN, POS_Y_TIME + TEXT_MARGIN, WIDTH_TIME - TEXT_MARGIN * 2, HEIGHT_TIME - TEXT_MARGIN * 2, TFT_BLACK);
+  display.setTextColor(TFT_WHITE);
+  display.setTextDatum(textdatum_t::middle_center);
+  display.drawString(time.substring(0, 10), img_x, POS_Y_TIME + HEIGHT_TIME / 2);
+}
+
+/**
+ * @brief 天気コード表示
+ *
+ * @param index 天気情報インデックス
+ * @param time 天気コード文字列
+ */
+void outputCode(int index, String code)
+{
+  int32_t img_x = (LCD_WIDTH / 4) + WIDTH_TIME * index;
+  // int32_t tw = display.textWidth(code);
+  display.fillRect(WIDTH_TIME * index + TEXT_MARGIN, POS_Y_CODE + TEXT_MARGIN, WIDTH_TIME - TEXT_MARGIN * 2, HEIGHT_CODE - TEXT_MARGIN * 2, TFT_BLACK);
+  display.setTextColor(TFT_WHITE);
+  display.setTextDatum(textdatum_t::middle_center);
+  display.drawString(code, img_x, POS_Y_CODE + HEIGHT_CODE / 2);
+}
+
+/**
+ * @brief 天気メッセージ表示
+ *
+ * @param index 天気情報インデックス
+ * @param weather_msg 天気メッセージ
+ */
+void outputMessage(int index, String weather_msg)
+{
+  int32_t th = display.fontHeight();
+  int32_t msg_y = POS_Y_MSG + TEXT_MARGIN;
+  int32_t msg_h = (LCD_HEIGHT - HEIGHT_TIME) - HEIGHT_IMAGE - msg_y;
+  display.fillRect(WIDTH_MSG * index + TEXT_MARGIN, msg_y, WIDTH_MSG - TEXT_MARGIN * 2, msg_h - TEXT_MARGIN * 2, TFT_BLACK);
+  // size_t l = strlen(weather_msg) / 3;
+  // Serial.println(l);
+  // String msg = String(weather_msg);
+  size_t l = weather_msg.length();
+  // Serial.println(l);
+  int j = 0;
+  display.setTextDatum(textdatum_t::top_left);
+  for (; j < l / MAXLEN_MSG; j++)
+  {
+    String s = weather_msg.substring(j * MAXLEN_MSG, j * MAXLEN_MSG + MAXLEN_MSG);
+    display.drawString(s, 2 + WIDTH_MSG * index, msg_y + j * th);
+    Serial.println(s.c_str());
+  }
+  if (l % MAXLEN_MSG != 0)
+  {
+    String s = weather_msg.substring(j * MAXLEN_MSG);
+    display.drawString(s, 2 + WIDTH_MSG * index, msg_y + j * th);
+    Serial.println(s.c_str());
+  }
+}
+
+/**
+ * @brief 天気アイコン表示
+ *
+ * @param index 天気情報インデックス
+ * @param weather_img_code アイコン用天気コード
+ */
+void outputImage(int index, uint16_t weather_img_code)
+{
+  int32_t img_x = (LCD_WIDTH / 4) + WIDTH_TIME * index;
+  const unsigned char *img_data = image_map[weather_img_code]; // img_data = { xx, xx, width lower, width higher, height lower, height higher }
+  int32_t img_w = (int)img_data[3] * 256 + (int)img_data[2];
+  int32_t img_h = (int)img_data[5] * 256 + (int)img_data[4];
+  int32_t img_y = (LCD_HEIGHT - 20) - 40;
+  display.setSwapBytes(true);                                                                      // バイト順の変換を有効にする。
+  display.pushImage(img_x - img_w / 2, img_y - img_h / 2, img_w, img_h, (uint16_t *)&img_data[8]); // RGB565の16bit画像データを描画。
+}
+
+char bufTime[32];
+/**
+ * @brief 時刻表示
+ *
+ * @param epoch 時刻（epoch time）
+ */
+void outputTime(time_t epoch)
+{
+  tm *t = localtime(&epoch);
+  snprintf(bufTime, sizeof(bufTime), "%04d/%02d/%02d %02d:%02d:%02d", t->tm_year + 1900, t->tm_mon + 1, t->tm_mday, t->tm_hour, t->tm_min, t->tm_sec);
+  int32_t th = display.fontHeight();
+  int32_t tw = display.textWidth(bufTime);
+  display.fillRect(LCD_WIDTH - tw, LCD_HEIGHT - th, tw, th, TFT_BLACK);
+  display.setTextColor(TFT_YELLOW);
+  display.setTextDatum(textdatum_t::bottom_right);
+  display.drawString(bufTime, LCD_WIDTH - 1, LCD_HEIGHT - 1);
+}
 
 time_t pre_epoch = timeClient.getEpochTime();
 bool first = true;
@@ -199,14 +310,7 @@ void loop()
     }
 
     // display time
-    tm *t = localtime(&epoch);
-    snprintf(bufTime, sizeof(bufTime), "%04d/%02d/%02d %02d:%02d:%02d", t->tm_year + 1900, t->tm_mon + 1, t->tm_mday, t->tm_hour, t->tm_min, t->tm_sec);
-    int32_t th = display.fontHeight();
-    int32_t tw = display.textWidth(bufTime);
-    display.fillRect(LCD_WIDTH - tw, LCD_HEIGHT - th, tw, th, TFT_BLACK);
-    display.setTextColor(TFT_YELLOW);
-    display.setTextDatum(textdatum_t::bottom_right);
-    display.drawString(bufTime, LCD_WIDTH - 1, LCD_HEIGHT - 1);
+    outputTime(epoch);
 
     // get weather information
     if (first || epoch % SECOONDS_AT_DAY == WEATHER_REQUEST_TIMING) // 22:30
@@ -235,101 +339,27 @@ void loop()
         {
           const JsonArray arrTimes = doc[0]["timeSeries"][0]["timeDefines"];
           size_t index = arrTimes.size() - 2;
-          Serial.println(index);
+          // Serial.println(index);
           for (int i = 0; i < 2; i++)
           {
-            const char *time = doc[0]["timeSeries"][0]["timeDefines"][i + index].as<const char *>();
+            const String time = String(doc[0]["timeSeries"][0]["timeDefines"][i + index].as<const char *>());
             Serial.print("time: ");
             Serial.println(time);
-            const char *weather_code = doc[0]["timeSeries"][0]["areas"][WEATHER_SUB_AREA_INDEX]["weatherCodes"][i + index].as<const char *>();
+            const String weather_code = String(doc[0]["timeSeries"][0]["areas"][WEATHER_SUB_AREA_INDEX]["weatherCodes"][i + index].as<const char *>());
             Serial.print("weatherCode: ");
             Serial.println(weather_code);
-            const char *weather_msg = doc[0]["timeSeries"][0]["areas"][WEATHER_SUB_AREA_INDEX]["weathers"][i + index].as<const char *>();
-            Serial.print("weather: ");
-            Serial.println(weather_msg);
+            const String weather_msg = String(doc[0]["timeSeries"][0]["areas"][WEATHER_SUB_AREA_INDEX]["weathers"][i + index].as<const char *>());
 
-            int32_t img_x = (LCD_WIDTH / 4) + (LCD_WIDTH / 2) * i;
             // time
-            char buf[10 + 1];
-            memcpy(buf, time, 10);
-            buf[10] = 0;
-            tw = display.textWidth(buf);
-            display.fillRect(2 + (LCD_WIDTH / 2) * i, 20 + 2, (LCD_WIDTH / 2) - 2 * 2, 20 - 2 * 2, TFT_BLACK);
-            display.setTextColor(TFT_WHITE);
-            display.setTextDatum(textdatum_t::middle_center);
-            display.drawString(buf, img_x, 30);
+            outputTime(i, time);
+            // code
+            outputCode(i, weather_code);
             // message
-            int32_t msg_y = 40 + 2;
-            int32_t msg_h = (LCD_HEIGHT - 20) - 80 - msg_y;
-            display.fillRect(2 + (LCD_WIDTH / 2) * i, msg_y, (LCD_WIDTH / 2) - 2 * 2, msg_h - 2 * 2, TFT_BLACK);
-            // size_t l = strlen(weather_msg) / 3;
-            // Serial.println(l);
-            String s = String(weather_msg);
-            size_t l = s.length();
-            Serial.println(l);
-            const int str_at_line = 3 * 7;
-            int j = 0;
-            display.setTextDatum(textdatum_t::top_left);
-            for (; j < l / str_at_line; j++)
-            {
-              display.drawString(s.substring(j * str_at_line, j * str_at_line + str_at_line), 2 + (LCD_WIDTH / 2) * i, msg_y + j * th);
-              Serial.println(s.substring(j * str_at_line, j * str_at_line + str_at_line).c_str());
-            }
-            if (l % str_at_line != 0)
-            {
-              display.drawString(s.substring(j * str_at_line), 2 + (LCD_WIDTH / 2) * i, msg_y + j * th);
-              Serial.println(s.substring(j * str_at_line).c_str());
-            }
+            outputMessage(i, weather_msg);
             // image
             const uint16_t weather_img_code = weatherMap[weather_code].as<const uint16_t>();
-            const unsigned char *img_data = image_map[weather_img_code]; // img_data = { xx, xx, width lower, width higher, height lower, height higher }
-            int32_t img_w = (int)img_data[3] * 256 + (int)img_data[2];
-            int32_t img_h = (int)img_data[5] * 256 + (int)img_data[4];
-            int32_t img_y = (LCD_HEIGHT - 20) - 40;
-            display.setSwapBytes(true);                                                                      // バイト順の変換を有効にする。
-            display.pushImage(img_x - img_w / 2, img_y - img_h / 2, img_w, img_h, (uint16_t *)&img_data[8]); // RGB565の16bit画像データを描画。
+            outputImage(i, weather_img_code);
           }
-          // Serial.print("time: ");
-          // Serial.println(time);
-          // const char *weather_code = doc[0]["timeSeries"][0]["areas"][WEATHER_SUB_AREA_INDEX]["weatherCodes"][WEATHER_TOMORROW_INDEX].as<const char *>();
-          // Serial.print("weatherCode: ");
-          // Serial.println(weather_code);
-          // const char *weather_msg = doc[0]["timeSeries"][0]["areas"][WEATHER_SUB_AREA_INDEX]["weathers"][WEATHER_TOMORROW_INDEX].as<const char *>();
-          // Serial.print("weather: ");
-          // Serial.println(weather_msg);
-          // char weather_code[16];
-          // snprintf(weather_code, sizeof(weather_code), "%d", code);
-          // th = display.fontHeight();
-          // tw = display.textWidth(weather_code);
-          // display.fillRect(0, 16, tw, th, TFT_BLACK);
-          // display.setTextColor(TFT_WHITE);
-          // display.setTextDatum(textdatum_t::top_left);
-          // display.drawString(weather_code, 0, 16);
-          // const uint16_t weather_img_code = weatherMap[weather_code].as<const uint16_t>();
-          // Serial.print("weather image code: ");
-          // Serial.println(weather_img_code);
-          // tw = display.textWidth("くもり"); // lgfxJapanGothic_16＝0x1002581d　lgfxJapanMincho_16＝0x1002597c 0x10025980 0x10025984
-          // tw = 6 * 8;
-          // tw = display.textWidth(weather_msg);
-          // display.fillRect(0, 32, tw, th, TFT_BLACK);
-          // display.drawString("くもり", 0, 32);
-          // display.drawString(weather_msg, 0, 32);
-
-          // const unsigned char *img_data = image_map[weather_img_code]; // img_data = { xx, xx, width lower, width higher, height lower, height higher }
-          // char buf[32];
-          // snprintf(buf, sizeof(buf), "%p", gImage_201);
-          // Serial.println(buf);
-          // snprintf(buf, sizeof(buf), "%p", img_data);
-          // Serial.println(buf);
-          // int32_t img_w = (int)img_data[3] * 256 + (int)img_data[2];
-          // int32_t img_h = (int)img_data[5] * 256 + (int)img_data[4];
-          // Serial.print("width: ");
-          // Serial.print(img_w);
-          // Serial.print(", height: ");
-          // Serial.print(img_h);
-          // Serial.println("pushImage");
-          // display.setSwapBytes(true);                                        // バイト順の変換を有効にする。
-          // display.pushImage(80, 80, img_w, img_h, (uint16_t *)&img_data[8]); // RGB565の16bit画像データを描画。
         }
         hcl.stop();
       }
